d1994d7f6f4269e7628e7ac8af31ade4661b2c14
Jchench <jessiechen2026@u.northwestern.edu>
2024-06-16 14:25:14 -0500

    worked on rolling average

--- 01_initial_setup.R
+++ 01_initial_setup.R
@@ -13,12 +13,17 @@ general_elections_2016 <-
   mutate(date_column = as.Date(date, format = "%m/%d") |> update(year = 2016)) |> 
   filter(date_column >= as.Date("2016-06-01") & date_column <= as.Date("2016-11-07"))
 
-seven_day_avg <- 
+polling_data <- 
   general_elections_2016 |> 
-  group_by(date_column) |> 
-  summarize(trump_mean = mean(trump_r),
-            clinton_mean = mean(clinton_d)) |> 
-  mutate(week = cut(date_column, breaks = seq(min(date_column), max(date_column) + 6, by = 7))) |> 
-  group_by(week) |> 
-  summarize(trump_mean = mean(trump_mean),
-            clinton_mean = mean(clinton_mean))
+  arrange(date_column) |> 
+  mutate(Clinton_7day_avg = rollapply(clinton_d, width = 7, FUN = mean, align = "right", fill = NA),
+         Trump_7day_avg = rollapply(trump_r, width = 7, FUN = mean, align = "right", fill = NA))
+
+ggplot(polling_data |> filter(str_detect(pollster, "^LA")), aes(x = date_column)) +
+  geom_line(aes(y = Clinton_7day_avg, color = "Clinton")) +
+  geom_line(aes(y = Trump_7day_avg, color = "Trump")) +
+  labs(title = "7-Day Rolling Average of Polling Results (June 1, 2016 - November 7, 2016)",
+       x = "Date",
+       y = "Support (%)",
+       color = "Candidate") +
+  theme_minimal()
